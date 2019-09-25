6144093085cb6486365ea24c49cf38f1cf36648a
Merlin Thankachan <51168425+merlinthankachan@users.noreply.github.com>
2019-09-25 12:34:51 -0400

    Update area_rectangle.cpp

--- area_rectangle.cpp
+++ area_rectangle.cpp
@@ -1,18 +1,24 @@
 // C++ program to find area of a rectangle//
 *********************************************
 
-LIBRARY ieee;
-USE ieee.std_logic_1164.ALL;
-entity bcd_7seg is
-port(q0,q1,q2,q3: in bit;a,b,c,d,e,f,g:out bit);
-end entity bcd_7seg;
-architecture operation of bcd_7seg is
-begin
-a<= (((not q2) and (not q0))or q1 or q3 or (q2 and q0));
-b<=(not q2 or ((not q1) and (not q0))or(q1 and q0));
-c<=((not q1) or q0 or q2);
-d<=(((not q2) and (not q0)) or (not q2 and q1) or (q2 and (not q1) and q0) or (q1 and (not q0))or q3);
-e<=((not q2 and (not q0))or (q1 and (not q0))); 
-f<=((not q1 and (not q0))or (q2 and (not q1))or (q2 and (not q0))or q3);
-g<=((not q2 and q1)or(q2 and (not q1))or q3 or(q2 and (not q0)));
-end architecture operation;
+#include <iostream> 
+using namespace std; 
+int rect(int, int); 
+float rect (double, double); 
+int main() 
+{ 
+int a, b; 
+double c, d; 
+cout<<"Enter the sides of the rectangle(whole number)"<<"\n"; 
+cin>>a>>b; 
+cout<<"Area = "<<rect(a, b); 
+cout<<"\n"<<"Enter the sides of the rectangle"<<"\n"; 
+cin>>c>>d; 
+cout<<"Area = "<<rect(c, d); 
+} int rect(int a, int b) 
+{ 
+return(a*b); 
+} float rect(double c, double d) 
+{ 
+return(c*d); 
+} 
