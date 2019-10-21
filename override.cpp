9daaa6e59c6e5f28ebc1fb23afe265a6577a9d86
Merlin Thankachan <51168425+merlinthankachan@users.noreply.github.com>
2019-10-21 13:32:46 -0400

    Update override.cpp

--- override.cpp
+++ override.cpp
@@ -1,4 +1,4 @@
- // Example of a Cpp program to  override the virtual function in  derived classes//
+ // Example of a Cpp program to create virtual function in a base class and to override the virtual function in  derived classes//
 
 #include<iostream> 
 using namespace std; 
@@ -7,11 +7,11 @@ class bas
 virtual void print () 
 { cout<< "The base class is printed" <<endl; } 
 void show () 
-{ cout<< "base class is shown" <<endl; } 
+{ cout<< " The base class is shown" <<endl; } 
 }; class der:public bas 
 { public: 
 void print () 
-{ cout<< "derived class is printed" <<endl; } 
+{ cout<< "The derived class is printed" <<endl; } 
 void show () 
 { cout<< "derived class is shown" <<endl; } 
 }; int main() 
