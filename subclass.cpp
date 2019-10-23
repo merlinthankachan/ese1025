5bef2e4a81c7f66d9863bd03f33187c957d25882
Merlin Thankachan <51168425+merlinthankachan@users.noreply.github.com>
2019-10-23 11:50:42 -0400

    Create subclass.cpp

---
+++ subclass.cpp
@@ -0,0 +1,29 @@
+ Program to derive the sub classes from the base classes using multiple inheritance? 
+#include <iostream> 
+using namespace std; 
+class student1{ 
+public: 
+int r; 
+void getexpense1() { 
+cout<<"enter the expense of student1"<<endl; 
+cin>>r; 
+} 
+}; 
+class student2 { 
+public: int s; 
+void getexpense2() { 
+cout<<"enter the expense of student2"<<endl; 
+cin>>s; 
+} 
+}; class totalexpense: public student1, public student2 { 
+public: 
+void gettotal() { 
+cout << "Total expense: " << s+r ; 
+} 
+}; int main() { 
+totalexpense obj; 
+obj.getexpense1(); 
+obj.getexpense2(); 
+obj.gettotal(); 
+return 0; 
+} 
