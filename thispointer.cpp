ae10e21218b8709a8ffa5150f98a78404bac3d79
Merlin Thankachan <51168425+merlinthankachan@users.noreply.github.com>
2019-11-04 10:03:29 -0500

    Create thispointer.cpp

---
+++ thispointer.cpp
@@ -0,0 +1,25 @@
+. Write a program to introduce a “this pointer” to access the private members of a class, 
+when local variable name is same as member’s name? 
+#include <iostream> 
+using namespace std; 
+class sample { 
+private: 
+int num; 
+char ch; 
+public: 
+void setvalues(char ch, int num) 
+{ 
+this->num =num; 
+this->ch=ch; 
+} 
+void displayvalues() 
+{ 
+cout<<ch<<endl; 
+cout<<num; 
+} }; int main() 
+{ 
+sample obj; 
+obj.setvalues( 'A',100); 
+obj.displayvalues(); 
+return 0; 
+} 
