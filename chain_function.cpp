cc24c7e0b21079afe7f5587cce1f24acbb8d8f27
Merlin Thankachan <51168425+merlinthankachan@users.noreply.github.com>
2019-11-03 20:22:47 -0500

    Update chain_function.cpp

--- chain_function.cpp
+++ chain_function.cpp
@@ -1,4 +1,4 @@
- A class level program with chain functions and constructor with parameters? 
+ //A class level program with chain functions and constructor with parameters//
 #include <iostream> 
 #include <math.h> 
 using namespace std; 
