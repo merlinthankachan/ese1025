cf17cef7fca0d0f8c4a80801848eb6996ad11aa2
Merlin Thankachan <51168425+merlinthankachan@users.noreply.github.com>
2020-04-18 23:02:20 -0400

    Update multiple inheritence.cpp

--- multiple inheritence.cpp
+++ multiple inheritence.cpp
@@ -1,5 +1,5 @@
 // Example of a  Cpp program to derive the sub classes from the base classes using multiple inheritance //
-**********************************************************************************************************
+***********************************************************************************************************
 
 #include <iostream> 
 using namespace std; 
